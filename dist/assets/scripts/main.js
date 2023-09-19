7506648f300964a7034d3402fe0c4785333c6ca9
tanabe1201 <stnb1201@gmail.com>
2023-09-19 19:33:18 +0900

    fix

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -14,13 +14,6 @@ $('a[href^="#"]').on('click', function () {
   }, 500);
   return false;
 });
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 600) {
-    $('.sc-inview').addClass('is-anim');
-  } else {
-    $('.sc-inview').removeClass('is-anim');
-  }
-});
 $("a[href*='http://']:not([href*='" + location.hostname + "']),[href*='https://']:not([href*='" + location.hostname + "'])").attr('target', '_blank').addClass('blank');
 var onLoad = document.getElementsByClassName("onload");
 window.addEventListener('load', function () {
@@ -47,35 +40,5 @@ tabTriggers.forEach(function (trigger) {
     });
     document.getElementById(trigger.dataset.tab).classList.add('is-active');
   });
-}); // // ブラウザ高さを取得し
-// var windowHeight = document.documentElement.clientHeight;
-// // ターゲット要素の高さを合わせる (box-sizing: border-box)
-// $('.l-side__list--scroll').height(windowHeight)
-// // ターゲット要素が、box-sizing: content-box の場合はこちら
-// $('.l-side__list--scroll').outerHeight(windowHeight)
-// var $target = $('.l-side__list--scroll');
-// // リサイズインベントで更新
-// $(window).resize(function() {
-//   $target.height( document.documentElement.clientHeight );
-// });
-// $(function(){
-//     //.sampleをクリックしてajax通信を行う
-//     $('.c-cont__link').click(function(){
-//         $.ajax({
-//             url: 'localhost:3000',
-//             /* 自サイトのドメインであれば、https://kinocolog.com/ajax/test.html というURL指定も可 */
-//             type: 'GET',
-//             dataType: 'html'
-//         }).done(function(data){
-//             /* 通信成功時 */
-//             $('.single').html(data); //取得したHTMLを.resultに反映
-//         }).fail(function(data){
-//             /* 通信失敗時 */
-//             alert('通信失敗！');
-//         });
-//     });
-// });
-// $(function() {
-//   $('.c-sec__header').matchHeight();
-// });
+});
 //# sourceMappingURL=main.js.map
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -18,14 +18,6 @@ $('a[href^="#"]').on('click', function () {
   return false;
 })
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 600) {
-    $('.sc-inview').addClass('is-anim');
-  } else {
-    $('.sc-inview').removeClass('is-anim');
-  }
-})
-
 $("a[href*='http://']:not([href*='" + location.hostname + "']),[href*='https://']:not([href*='" + location.hostname + "'])").attr('target', '_blank').addClass('blank');
 
 const onLoad = document.getElementsByClassName("onload");
@@ -40,8 +32,6 @@ window.addEventListener('load', function(){
     document.querySelector("body").classList.toggle("scroll_on");
   },800);
 });  
-
-
 
 var tabTriggers = document.querySelectorAll('.js-tab--trigger');
 var tabContents = document.querySelectorAll('.js-tab--content');
@@ -58,46 +48,3 @@ tabTriggers.forEach(function (trigger) {
     document.getElementById(trigger.dataset.tab).classList.add('is-active');
   });
 });
-// // ブラウザ高さを取得し
-// var windowHeight = document.documentElement.clientHeight;
-
-// // ターゲット要素の高さを合わせる (box-sizing: border-box)
-// $('.l-side__list--scroll').height(windowHeight)
-
-// // ターゲット要素が、box-sizing: content-box の場合はこちら
-// $('.l-side__list--scroll').outerHeight(windowHeight)
-
-
-// var $target = $('.l-side__list--scroll');
-
-// // リサイズインベントで更新
-// $(window).resize(function() {
-
-//   $target.height( document.documentElement.clientHeight );
-
-// });
-
-
-// $(function(){
-//     //.sampleをクリックしてajax通信を行う
-//     $('.c-cont__link').click(function(){
-//         $.ajax({
-//             url: 'localhost:3000',
-//             /* 自サイトのドメインであれば、https://kinocolog.com/ajax/test.html というURL指定も可 */
-//             type: 'GET',
-//             dataType: 'html'
-//         }).done(function(data){
-//             /* 通信成功時 */
-//             $('.single').html(data); //取得したHTMLを.resultに反映
-            
-//         }).fail(function(data){
-//             /* 通信失敗時 */
-//             alert('通信失敗！');
-                    
-//         });
-//     });
-// });
-
-// $(function() {
-//   $('.c-sec__header').matchHeight();
-// });
